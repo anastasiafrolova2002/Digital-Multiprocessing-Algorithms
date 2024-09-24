1ddc362ab747acdf7d25bb3b9856968620f29bc2
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-09-24 09:34:48 +0300

    Create lab2_4_5.py

---
+++ lab2_4_5.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, (140,50,120), (179,255,255))
+    #mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    moments = cv2.moments(mask)
+    area = moments['m00']
+
+    if area > 0:
+        width = height = int(np.sqrt(area))
+        c_x = int(moments["m10"] / moments["m00"])
+        c_y = int(moments["m01"] / moments["m00"])
+        cv2.rectangle(frame,
+            (c_x - (width // 12), c_y - (height // 12)),
+            (c_x + (width // 12), c_y + (height // 12)),
+            (0, 0, 0), 2)
+
+    cv2.imshow('Rectanle_frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
